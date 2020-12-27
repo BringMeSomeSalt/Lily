834fbb64ba24ece974c0a96a5b497e96a7c33f1d
BringMeSomeSalt <71334435+BringMeSomeSalt@users.noreply.github.com>
2020-12-27 20:04:18 +0700

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
 
 const Embed = new Discord.MessageEmbed()
   .setTitle(`uh...`)
-  .setDescription(`Type Lpoll<number between 1 - 10> to make poll has (number you type) reactions. eg. Lpoll2 Do you like cake?                                                                          1:yes                                                                               2:no`)
+  .setDescription(`Type Lpoll<number between 1 - 10> to make poll has (number you type) reactions. eg. Lpoll2 Do you like cake? 1: yes - 2: no`)
   .setColor(`#80dfff`);
   let msgEmbed = await message.channel.send(Embed);
 }
