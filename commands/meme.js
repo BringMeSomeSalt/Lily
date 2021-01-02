90a5c6659c8d08600272694bc6f076427a5b6d0f
BringMeSomeSalt <71334435+BringMeSomeSalt@users.noreply.github.com>
2021-01-02 22:25:37 +0700

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
         fetch('https://meme-api.herokuapp.com/gimme')
             .then(res => res.json())
             .then(async json => {
-                let msg = await message.channel.send('Sending a meme...');
+                let msg = await message.channel.send('Meme!');
                 const memeEmbed = new Discord.MessageEmbed()
                     .setTitle("Meme!")
                     .setImage(json.url)
