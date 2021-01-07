be4ecd24095acff65783219558c3ea043644f6bf
BringMeSomeSalt <71334435+BringMeSomeSalt@users.noreply.github.com>
2021-01-08 00:05:28 +0700

    d

--- commands/hmmm.js
+++ commands/hmmm.js
@@ -1,19 +1,16 @@
-const Discord = require('discord.js');
-const randomPuppy = require('random-puppy');
+const { MessageEmbed } = require("discord.js");
+const api = require("imageapi.js");
 
 module.exports.run = async (bot, message, args) => {
-    const subReddits = ["hmmm"]
-    const random = subReddits[Math.floor(Math.random() * subReddits.length)];
-    const img = await randomPuppy(random);
-
-    const embed = new Discord.MessageEmbed()
-    .setImage(img)
-    .setURL(`http://reddit.com/${random}`)
-    .setColor(`#ff3399`)
-
-    message.channel.send(embed);
-
-}
+        let subreddits = ["hmmm"];
+        let subreddit = subreddits[Math.floor(Math.random() * subreddits.length)];
+        let img = await api(subreddit, true);
+        const Embed = new MessageEmbed()
+          .setTitle(`Hmmmmm`)
+          .setColor("RANDOM")
+          .setImage(img);
+        message.channel.send(Embed);
+      }
 
 module.exports.config = {
     name: "Lhmmm",
