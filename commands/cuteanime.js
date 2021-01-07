a61ae7770adab45eedc49ae59450ecbe3ed2fdb8
BringMeSomeSalt <71334435+BringMeSomeSalt@users.noreply.github.com>
2021-01-07 23:54:20 +0700

    Update cuteanime.js

--- commands/cuteanime.js
+++ commands/cuteanime.js
@@ -1,19 +1,19 @@
-const { MessageEmbed } = require('discord.js');
-const somethingRandom = require('some-random-cat').Random
+const Discord = require('discord.js');
+const randomPuppy = require('random-puppy');
 
 module.exports.run = async (bot, message, args) => {
-        const subreddits = ["awwnime"]
-        let randomSubReddit = subreddits[Math.floor(Math.random() * subreddits.length)]
-        somethingRandom.getMeme(randomSubReddit).then(res => {
-            const embed = new MessageEmbed()
-                .setTitle("Cute Anime!")
-                .setImage(res.img)
-                .setFooter(`👍 ${res.upvotes} | 👎 ${res.downvotes} | 💬 ${res.comments}`)
-                .setColor('RANDOM')
-            message.channel.send(embed)
-        }).catch(e => message.channel.send('API Error. Please try again.'))
-    }
+    const subReddits = ["awwnime"]
+    const random = subReddits[Math.floor(Math.random() * subReddits.length)];
+    const img = await randomPuppy(random);
 
+    const embed = new Discord.MessageEmbed()
+    .setImage(img)
+    .setURL(`http://reddit.com/${random}`)
+    .setColor(`#ff3399`)
+
+    message.channel.send(embed);
+
+}
     module.exports.config = {
         name: "Lcuteanime",
         description: "example of an help.",
