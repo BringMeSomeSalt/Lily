0e74ad0cc84ac040b519a7397da3155d2c752789
BringMeSomeSalt <71334435+BringMeSomeSalt@users.noreply.github.com>
2021-01-13 23:20:33 +0700

    h

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,10 +2,10 @@ const superagent = require("snekfetch");
 const Discord = require('discord.js')
 
 module.exports.run = async (bot, message, args) => {
-    superagent.get('https://nekos.life/api/v2/img/kemonomimi')
+    superagent.get('https://nekos.life/api/v2/img/avatar')
         .end((err, response) => {
       const embed = new Discord.MessageEmbed()
-      .setTitle("Kemonomimi!")
+      .setTitle("Cat!")
       .setImage(response.body.url)
       .setColor("#80dfff")
       .setURL(response.body.url);
@@ -18,9 +18,9 @@ module.exports.run = async (bot, message, args) => {
 }
 
 module.exports.config = {
-    name: "Lkemonomimi",
+    name: "Lavatar",
     description: "",
-    usage: "Lkemonomimi",
+    usage: "Lavatar",
     accessableby: "Members",
     aliases: []
 }
